a8fe5afd84fbf781e4631af96d8a0d918d68e2fc
hota23 <137266314+hota23@users.noreply.github.com>
2024-01-14 20:44:53 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+// Reference (Non primitive)
+
+// Arrays, Objects, Functions   
