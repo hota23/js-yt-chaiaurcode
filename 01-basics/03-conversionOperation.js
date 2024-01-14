96da5a1c33c4dfd9d5b89a1911cf4305296fae1f
hota23 <137266314+hota23@users.noreply.github.com>
2024-01-14 09:41:15 +0000

    operations

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -29,7 +29,7 @@ let stringNumber = String(someNumber)
 
 // *********************** Operations ***********************
 
-let value = 3
+let value = 8
 let negValue = -value
 // console.log(negValue);
 
@@ -46,23 +46,21 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
 
-// console.log( (3 + 4) * 5 % 3);
-
-// console.log(+true);
-// console.log(+"");
+// console.log(true) 
+// console.log(+true)  
+// console.log(+"")
 
 let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2+2
 
-let gameCounter = 100
+let gameCounter = 100;
 ++gameCounter;
 console.log(gameCounter);
 
-// link to study
-// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// study : ecmascript type conversion abstract operations
