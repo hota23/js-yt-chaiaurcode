9f9b1fbee456dd8dfa7292b7fb4f40ab00b8410c
hota23 <137266314+hota23@users.noreply.github.com>
2024-01-16 13:04:09 +0000

    datatype-summary

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -13,6 +13,23 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId)
 
+// const bigNumber = 5641564561555577799n
+
 // Reference (Non primitive)
 
 // Arrays, Objects, Functions   
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "raj",
+    age: 89,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof outsideTemp)
+console.log(typeof anotherId)
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
