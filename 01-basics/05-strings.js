e407e9c3795c35fb6ad08394da8c741c5868adce
hota23 <137266314+hota23@users.noreply.github.com>
2024-01-17 15:31:42 +0000

    strings

---
+++ 01-basics/05-strings.js
@@ -0,0 +1,22 @@
+const name = "raj"
+const repoCount = 1
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('rajHOTA')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('a'))
+console.log(gameName.indexOf('x'))
+
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
