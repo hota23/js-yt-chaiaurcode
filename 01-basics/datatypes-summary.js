bfb60f95936452639ff1cb575f1baab11cea2823
hota23 <137266314+hota23@users.noreply.github.com>
2024-01-17 15:09:08 +0000

    stack vs heap memory

--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -41,3 +41,21 @@ console.log(typeof anotherId)
 // Stack (Primitive), Heap (Non-Primitive)
 
 let myName = "RajHota"
+
+let anotherName = myName
+anotherName = "bidyaratnaHota"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "raj@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
