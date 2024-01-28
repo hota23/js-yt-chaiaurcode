8d97b17dbda90734b5270931423d4a7a93770745
hota23 <137266314+hota23@users.noreply.github.com>
2024-01-28 10:10:22 +0000

    strings finished

--- 01-basics/05-strings.js
+++ 01-basics/05-strings.js
@@ -5,7 +5,7 @@ const repoCount = 1
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
-const gameName = new String('hiteshchoudhary')
+const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0])
 // console.log(gameName.__proto__)
@@ -28,7 +28,9 @@ console.log(newStringOne)
 console.log(newStringOne.trim())
 
 const url = "https://hiteshchoudhary/hitesh%20choudhary"
-console.log(url.replace('%20','-'))
+console.log(url.replace('%20','-'));
+console.log(url.includes('hitesh'));
+console.log(url.includes('sundar'));
 
-
+console.log(gameName.split('-'));
 
