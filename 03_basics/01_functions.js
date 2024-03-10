690688f4388d89c51cadd6ed12705909ba399437
hota23 <137266314+hota23@users.noreply.github.com>
2024-03-10 15:00:23 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,7 +33,36 @@ function logInUserMessage(username = "sam"){
 }
 // console.log(logInUserMessage("raj"))
 // console.log(logInUserMessage(""))
-console.log(logInUserMessage("raj"))
+// console.log(logInUserMessage("raj"))
 
 
 
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 600))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 500, 600, 70000]))
