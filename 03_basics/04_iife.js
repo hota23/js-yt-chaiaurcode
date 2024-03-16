b3d74a30e2ae820392fd2dfe6ecdb093d4329fba
hota23 <137266314+hota23@users.noreply.github.com>
2024-03-16 15:38:49 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -2,7 +2,7 @@
 
 (function chai(){
     console.log(`DB CONNECTED`);
-})();//need the semicolon here to end the function
+})();//need the semicolon here to end the function if there are more than one iifes
 
 // () ()
 //used to remove the pollution from global variable
