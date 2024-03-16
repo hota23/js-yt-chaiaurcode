1115756b2917279fc6292c107194f576c071b3bd
hota23 <137266314+hota23@users.noreply.github.com>
2024-03-16 15:11:44 +0000

    THIS & arrow fn

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,52 @@
+const user = {
+    username: "raj",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} welcome to website`);
+        console.log(this)
+    } 
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai(){
+//     let username = "raj"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "raj"
+//     console.log(this.username);
+// }
+
+const chai = () => {
+    let username = "raj"
+    console.log(this);
+}
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)
+//if curly braces are used then you need to write return. if parantheses are used then you do not need to write return(implicit return).
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+//need to wrap objects in parentheses to return them like this.
+
+
+console.log(addTwo(3, 4))
+
+
+const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
