bcd70e566ddc3de0d2b90e31c724970ace519f80
hota23 <137266314+hota23@users.noreply.github.com>
2024-03-16 14:44:13 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,12 +6,57 @@ if (true) {
     const b = 20
     // var c = 30
     c = 30
-    console.log("INNER:", a)
+    // console.log("INNER:", a)
     
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "raj"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username)
+
+
+
+//++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+
